8acb8d29b648eae9f89f4649d4c386fd662ebc4b
Ritchie Xia <ritchie.xia@gmail.com>
2022-06-26 14:47:55 -0700

    Created bottom navigation bar

--- frontend/app/src/main/java/com/example/recipez/LoginActivity.java
+++ frontend/app/src/main/java/com/example/recipez/LoginActivity.java
@@ -12,6 +12,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
     final static String TAG = "LoginActivity";
@@ -30,6 +32,8 @@ public class LoginActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         // Check for existing Google Sign In account, if the user is already signed in
         // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
@@ -38,17 +42,58 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(mainActivityIntent);
         }
 
-        // signInButton = findViewById()
-//         signInButton.setOnClickListener(new View.OnClickListener() {
-//             @Override
-//             public void onClick(View view) {
-//                 signIn();
-//             }
-//         });
+         findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 signIn();
+             }
+         });
     }
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+            if (requestCode == RC_SIGN_IN) {
+                // The Task returned from this call is always completed, no need to attach
+                // a listener.
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+            }
+        }
+
+        private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+            try {
+                GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+                // Signed in successfully, show authenticated UI.
+                updateUI(account);
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                updateUI(null);
+            }
+        }
+
+        private void updateUI(GoogleSignInAccount account) {
+            if (account == null) {
+                Log.d(TAG, "There is no user signed in!");
+            } else {
+                // sign in was successful
+                Log.d(TAG, "Pref Name: " + account.getDisplayName());
+                Log.d(TAG, "Email Name: " + account.getEmail());
+                // Log.d(TAG, "Given Name: " + account.getGivenName());
+                // Log.d(TAG, "Family Name: " + account.getFamilyName());
+                // Log.d(TAG, "Display Picture: " + account.getPhotoUrl());
+                Intent mainActivityIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        }
 }
---
+++ frontend/app/src/main/java/com/example/recipez/ProfileActivity.java
@@ -0,0 +1,5 @@
+package com.example.recipez;
+
+public class ProfileActivity {
+
+}
