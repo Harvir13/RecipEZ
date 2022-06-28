fa7afaf02cde0b89d8e38ee45d48d434f2d32a06
Ritchie Xia <ritchie.xia@gmail.com>
2022-06-27 23:57:42 -0700

    Set up fragment structure in MainActivity

--- frontend/app/src/main/java/com/example/recipez/MainActivity.java
+++ frontend/app/src/main/java/com/example/recipez/MainActivity.java
@@ -1,22 +1,55 @@
 package com.example.recipez;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment fragment = new RecipesFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.recipes:
+                        fragment = new RecipesFragment();
+                        break;
+                    case R.id.profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
+                return true;
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fridgeFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new FridgeFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
+            }
+        });
     }
 }
