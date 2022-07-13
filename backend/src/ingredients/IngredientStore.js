c75a7c037cb3de9ebe92666339cfe3ae4ace1ce4
Harvir Dhaliwal <harvir.dhaliwal@hotmail.com>
2022-07-12 18:25:49 -0700

    added backend endpoint as requested by Faridah for grading notifications

--- backend/src/ingredients/IngredientStore.js
+++ backend/src/ingredients/IngredientStore.js
@@ -1,11 +1,22 @@
 import express from "express";
 import fetch from "node-fetch";
+// import admin from "firebase-admin";
+// import { createRequire } from 'module';
 
-const API_KEY = "1b961ea726c449868f6bffe1dd76da71";
+const API_KEY = "c3f5daa174074a7994e8e6803d3b2687";
+// var registrationToken = "dQeP0tUfTCmXF_TUVvk615:APA91bFbY6JuIRlxVzPv4EDvzkP1ubgQJm7VEplUpNCm1jedXaNEzrbad9Bs0DnmTKB2TbjLlgKqZj47Hm4lRyK2jZB5aHCa5N7iGiVDk9eJ7xF2QfiVeTEtpYJ0qyHYZ1pi6efATAms";
 const SERVER_KEY = "key=AAAAMKdSYCY:APA91bFkZgU98nuuyEQod_nkkfKP4U6r3uA-avUnsJu9oNYTw1T3MRgbaZ-pzeDgRkNKJomwiC9LMrvqYKVnkzOZPz5HJDk4Mm96l2E3epm4_ZFVCXBjQMVk4sXV78-H6qVT9voEKfrM";
 
 var app = express();
 app.use(express.json());
+
+
+// const require = createRequire(import.meta.url);
+// var serviceAccount = require("./recipez-92366-firebase-adminsdk-i7kge-e682dcca8c.json");
+
+// admin.initializeApp({
+// 	credential: admin.credential.cert(serviceAccount),
+//   })
 
 async function run() {
   	try {
@@ -22,7 +33,13 @@ run();
 
 setInterval(function() {
 	sendExpiryNotification();
-}, 10000)
+}, 300000)
+
+app.get("/getNotification", async(req, res) => {
+	console.log("here")
+	sendExpiryNotification();
+	res.send({"result": "Sent notificaiton"})
+})
 
 //expects {userid: xxx}
 app.get("/requestIngredients", async (req, res) => {
@@ -125,6 +142,10 @@ app.get("/requestExpiryDate", async (req, res) => {
 			let desiredIngredient;
 			data.forEach((ingredient) => {
 				let generalName = ingredient.name;
+				//let generalName = ingredient.name.split('-')
+				//console.log(generalName[0].trim().toLowerCase())
+				//console.log(req.query["ingredient"].toLowerCase())
+				//console.log(generalName)
 				let levDistance = levenshtein_distance(generalName, req.query["ingredient"]);
 				console.log("" + generalName + " : " + levDistance);
 				if (levDistance < minLevDistance) {
@@ -164,7 +185,7 @@ app.post("/addIngredient", async (req, res) => {
 		}).then((response) => response.json()).then((spoonacularIng) => {
 			console.log(spoonacularIng);
 			if (spoonacularIng.length == 0) {
-				res.send("No ingredient found");
+				res.send({"result": "No ingredient found"});
 				return;
 			}
 			let ingredient = {
@@ -182,6 +203,7 @@ app.post("/addIngredient", async (req, res) => {
 				},
 				body: JSON.stringify(ingredient)
 			}).then((response) => response.json()).then((data) => {
+				console.log(data)
 				res.send(data.ingredient);
 			});
 		});
@@ -190,7 +212,7 @@ app.post("/addIngredient", async (req, res) => {
 	}
 });
 
-// expects {time: xxx}
+// expects {userid: xxx, time: xxx}
 app.get("/scanExpiryDates", async (req, res) => {
 	try {
 		fetch("http://20.53.224.7:8085/usersWithExpiringIngredients?time=" + req.query["time"],
@@ -207,7 +229,7 @@ app.get("/scanExpiryDates", async (req, res) => {
 	}
 });
 
-// expects {userid: xxx, time: xxx}
+// expects {userid: xxx, time: zzz}
 app.get("/expiringIngredients", async (req, res) => {
     try {
         fetch("http://20.53.224.7:8085/getIngredients?userid=" + req.query["userid"],
@@ -235,7 +257,7 @@ app.get("/expiringIngredients", async (req, res) => {
     }
 });
 
-// expects { {restrictions: xxx, xxx, xxx}, ingredient: xxx}
+// expects { {restrictions: xxx, yyy, zzz}, ingredient: xxx}
 app.get("/checkDietaryRestrictions", async (req, res) => {
 	let restrictions = req.query["restrictions"].split(",");
 	let ingredientRestricted = false;
@@ -271,6 +293,7 @@ function levenshtein_distance(inputString, realString) {
 // checks expiry dates and sends a notification to the user, if something is expiring
 function sendExpiryNotification(res) {
 	let currTime = Math.round(Date.now() / 1000).toString();
+	// let currTime = Date.now().toString()
 	console.log("Current time: " + currTime)
 	fetch("http://20.53.224.7:8086/scanExpiryDates?time=" + currTime, {
 		method: "GET",
@@ -303,11 +326,13 @@ function sendExpiryNotification(res) {
 				}).then((response) => response.json()).then((ingredient) => {
 					console.log("Expiring Ingredients:" + ingredient)
 					var currToken = tokens.find((pair) => pair.userID == user).token
+					// var currToken = "evEPuCdDRUiWjsrGpNqMsX:APA91bHB-sNoz-fEfiECbytahF6GoWStdqgVtg6COm3gwzkCkqSrm0Zhlpovs9Wk-8cHr_2SZt9QtuYpb7PQgY3rINkL1a_G0OrJPP2iiZU7jGTtabdftcwaYg4fZI1VKNi8MG58XeEe"
 					var expiring = ""
 					for (let i = 0; i < ingredient.length; i++) {
 						expiring = expiring + ingredient[i]["name"] + ","
 					}
 					expiring = expiring.slice(0,-1)
+					console.log("userid: " + user, "ingredients: " + expiring)
 					var json = {
 						"data": {
 							"ingredients": expiring
@@ -322,10 +347,46 @@ function sendExpiryNotification(res) {
 						},
 						body: JSON.stringify(json)
 					}).then((response) => response.text()).then((data) => {
+						console.log(user)
 						console.log(data);
 					});
+					// admin.messaging().sendToDevice(currToken.toString(), {"data": {"ingredients": ingredient.toString()}}, {
+					// 	priority: "high",
+					// 	timeToLive: 60 * 60 * 24
+					//   })
+					// .then( response => {
+					// 	console.log(response)
+					// 	console.log("Send to: " + currToken.toString())
+					// })
+					// .catch( error => {
+					// 	console.log(error);
+					// });
+
 				})
 			})
 		})
+		
+        // let json = {
+        //     "data": {
+        //         "ingredients": data
+        //     },
+        //     "to": token
+        // }
+        // fetch("https://fcm.googleapis.com/fcm/send", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-type": "application/json",
+        //         Authorization: SERVER_KEY
+        //     },
+        //     body: JSON.stringify(json)
+        // }).then((response) => response.text()).then((data) => {
+        //     console.log(data);
+        // });
 	});
 }
+
+// DO WE STILL NEED THIS
+app.get("/test", async (req, res) => {
+	let dist = levenshtein_distance(req.query["input"], req.query["word"]);
+	res.send(dist.toString());
+});
