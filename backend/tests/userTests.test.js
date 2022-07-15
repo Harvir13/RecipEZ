281267182ddffe910ed532fca85ed4527a9b1230
ritchiexia <64629481+ritchiexia@users.noreply.github.com>
2022-07-14 22:38:39 -0700

    Update backend/tests/userTests.test.js
    Co-authored-by: Harvir14 <69890979+Harvir14@users.noreply.github.com>

--- backend/tests/userTests.test.js
+++ backend/tests/userTests.test.js
@@ -28,7 +28,7 @@ test('Store user token', () => {
     return axios.post(UserStoreURL + '/storeUserToken', {
       method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+        "Content-Type": "application/json",
         },
         body: JSON.stringify(req)
     }).then(response => expect(response).toEqual(res));
