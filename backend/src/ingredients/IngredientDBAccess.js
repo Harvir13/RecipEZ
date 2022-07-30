f7c9b2ab3bec1f28b51fe1a60a8034bd19f8e3c3
sandonli <sandonli@outlook.com>
2022-07-30 17:43:52 -0600

    Updated backend code

--- backend/src/ingredients/IngredientDBAccess.js
+++ backend/src/ingredients/IngredientDBAccess.js
@@ -1,80 +1,32 @@
-// import express from "express";
-// import { MongoClient } from "mongodb";
-const express = require('express')
 const {MongoClient} = require('mongodb')
-
-var app = express();
-app.use(express.json());
 
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
-async function run() {
-    await client.connect();
-    var server = app.listen(8085, (req, res) => {
-        var host = server.address().address;
-        var port = server.address().port;
-        console.log("Example server successfully running at http://%s:%s", host, port);
-    });
-}
-
-run();
-
-// expects {userid: xxx}
-app.get("/getIngredients", async (req, res) => {
-    client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(req.query["userid"], 10) }).then((result) => {
-        if (result === null) {
-            res.status(404).send(new Error("Error: invalid userID"));
-        }
-        else {
-            console.log(result.ingredients)
-            res.send(result.ingredients);
-        }
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
-});
+module.exports = {changeExpiry, usersWithExpiringIngredients, removeIngredient, storeIngredient, getIngredients, client};
 
 function getIngredients(userid) {
     return new Promise((resolve, reject) => {
         client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(userid, 10) }).then((result) => {
             if (result === null) {
-                console.log("result is empty")
-                return resolve({"status": 200, "result": []})
+                return reject({"status": 404, "result": "Error: invalid userID"});
             }
             else {
-                console.log(result.ingredients)
                 return resolve({"status": 200, "result": result.ingredients});
             }
         }).catch(err => {
             console.log(err)
             return reject({"status": 400, "result": err})
-        }) 
-    })
-}
-
-
-// expects body of {userid: xxx, ingredient: {name: xxx, expiry: yyy, image: zzz}}
-app.post("/storeIngredient", async (req, res) => {
-    if (req.body.ingredient.expiry < 0) return res.status(405).send(new Error("Error: invalid expiry value"));
-    client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(req.body.userid, 10) }).then((result) => {
-        if (result == null) return res.status(404).send(new Error("Error: invalid userID"));
-        let alreadyHaveIng = false;
-        result.ingredients.forEach((ingredient => {
-            if (ingredient.name == req.body.ingredient.name) alreadyHaveIng = true;
-        }));
-        if (!alreadyHaveIng) result.ingredients.push(req.body.ingredient);
-        let newIngredients = {$set: { ingredients: result.ingredients }};
-        client.db("IngredientDB").collection("Users").updateOne({ userid: parseInt(req.body.userid, 10) }, newIngredients).then(res.send(req.body));
-    }).catch((err) => {
-        res.status(400).send(err);
+        });
     });
-});
+}
 
 function storeIngredient(userid, inputIngredient) {
     return new Promise((resolve, reject) => {
+        if (inputIngredient.expiry < 0) return reject({"status": 405, "result": "Error: invalid expiry value"});
         client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(userid, 10) }).then((result) => {
             let alreadyHaveIng = false;
+            if (result == null) return reject ({"status": 404, "result": "Error: invalid userID"});
             result.ingredients.forEach((ingredient => {
                 if (ingredient.name == inputIngredient.name) alreadyHaveIng = true;
             }));
@@ -82,65 +34,34 @@ function storeIngredient(userid, inputIngredient) {
             let newIngredients = {$set: { ingredients: result.ingredients }};
             client.db("IngredientDB").collection("Users").updateOne({ userid: parseInt(userid, 10) }, newIngredients).then(result => {
                 return resolve({"status": 200, "result": inputIngredient});
-            })
+            });
         }).catch(err => {
             console.log(err)
             return reject({"status": 400, "result": err})
         }) 
     })
 }
-
-// expects body of {userid: xxx, ingredient: xxx}
-app.delete("/removeIngredient", async (req, res) => {
-    client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(req.body.userid, 10) }).then((result) => {
-        if (result == null) return res.status(404).send(new Error("Error: invalid userID"));
-        let newIngredients = result.ingredients.filter(function (value) {
-            return value.name != req.body.ingredient;
-        });
-    let updateString = {$set: { ingredients: newIngredients }};
-    client.db("IngredientDB").collection("Users").updateOne({ userid: parseInt(req.body.userid, 10) }, updateString).then((data) => res.send(data))
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
-});
 
 function removeIngredient(userid, ingredient) {
     return new Promise((resolve, reject) => {
         client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(userid, 10) }).then((result) => {
+            if (result == null) return reject ({"status": 404, "result": "Error: invalid userID"});
             let newIngredients = result.ingredients.filter(function (value) {
                 return value.name != ingredient;
             });
         let updateString = {$set: { ingredients: newIngredients }};
-        client.db("IngredientDB").collection("Users").updateOne({ userid: parseInt(userid, 10) }, updateString).then((data) => {
-            return resolve({"status": 200, "result": "Successfully deleted ingredient"})
+        client.db("IngredientDB").collection("Users").updateOne({ userid: parseInt(userid, 10) }, updateString).then(() => {
+            return resolve({"status": 200, "result": "Successfully deleted ingredient"});
         })
         }).catch(err => {
-            console.log(err)
             return reject({"status": 400, "result": err})
-        }) 
-    })
-}
-
-// expects {time: xxx}
-app.get("/usersWithExpiringIngredients", async (req, res) => {
-    if (req.query["time"] < 0) return res.status(405).send(new Error("Error: invalid expiry value"));
-    let expiringUsers = []
-    client.db("IngredientDB").collection("Users").find().toArray().then((result) => {
-        result.forEach((user) => {
-            let hasExpiring = false;
-            user.ingredients.forEach((ingredient) => {
-                if (ingredient.expiry <= req.query["time"] + (86400 * 2)) hasExpiring = true;
-            });
-            if (hasExpiring) expiringUsers.push(user.userid);
         });
-        res.send(expiringUsers);
-    }).catch((err) => {
-        res.status(400).send(err);
     });
-})
+}
 
 function usersWithExpiringIngredients(time) {
     return new Promise((resolve, reject) => {
+        if (time < 0) return reject({"status": 405, "result": "Error: invalid expiry value"});
         let expiringUsers = []
         client.db("IngredientDB").collection("Users").find().toArray().then((result) => {
             result.forEach((user) => {
@@ -158,26 +79,11 @@ function usersWithExpiringIngredients(time) {
     })
 }
 
-// expects {userid: xxx, ingredient: xxx, expiry: xxx}
-app.post("/changeExpiry", async (req, res) => {
-    if (req.body.expiry < 0) return res.status(405).send(new Error("Error: invalid expiry value"));
-    client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(req.body.userid, 10) }).then((result) => {
-        if (result == null) return res.status(404).send(new Error("Error: invalid userID"));
-        result.ingredients.forEach(function (value) {
-            if (value.name == req.body.ingredient) {
-                value.expiry = parseInt(req.body.expiry, 10);
-            }
-        });
-        let updateString = {$set: { ingredients: result.ingredients }};
-        client.db("IngredientDB").collection("Users").updateOne({ userid: parseInt(req.body.userid, 10) }, updateString).then(res.send(req.body))
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
-});
-
 function changeExpiry(userid, ingredient, expiry) {
     return new Promise((resolve, reject) => {
+        if (expiry < 0) return reject({"status": 405, "result": "Error: invalid expiry value"});
         client.db("IngredientDB").collection("Users").findOne({ userid: parseInt(userid, 10) }).then((result) => {
+            if (result == null) return reject ({"status": 404, "result": "Error: invalid userID"});
             result.ingredients.forEach(function (value) {
                 if (value.name == ingredient) {
                     value.expiry = parseInt(expiry, 10);
@@ -193,5 +99,3 @@ function changeExpiry(userid, ingredient, expiry) {
         }) 
     })
 }
-
-module.exports = {changeExpiry, usersWithExpiringIngredients, removeIngredient, storeIngredient, getIngredients}
