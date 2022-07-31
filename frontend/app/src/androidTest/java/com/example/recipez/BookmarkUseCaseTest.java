70c06843d83ee17a25eb21f5b08213bab8ba2ec6
jessicahe21 <jessicahe21@hotmail.com>
2022-07-31 14:50:17 -0400

    omg the bookmark tests finally all work

--- frontend/app/src/androidTest/java/com/example/recipez/BookmarkUseCaseTest.java
+++ frontend/app/src/androidTest/java/com/example/recipez/BookmarkUseCaseTest.java
@@ -29,10 +29,14 @@ import android.view.WindowManager;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.Root;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -115,6 +119,11 @@ public class BookmarkUseCaseTest {
         onView(withId(R.id.recipes)).perform(click());
         onView(withId(androidx.appcompat.R.id.search_src_text)).perform(replaceText("asdfasdf"), closeSoftKeyboard());
         onView(allOf(withId(androidx.appcompat.R.id.search_src_text), withText("asdfasdf"), childAtPosition(allOf(withId(androidx.appcompat.R.id.search_plate), childAtPosition(withId(androidx.appcompat.R.id.search_edit_frame), 1)), 0))).perform(pressImeActionButton());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(allOf(withId(R.id.recipe_card), childAtPosition(childAtPosition(withId(R.id.recipe_fragment_recycler_view), 0), 0))).check(doesNotExist());
         onView(withText("No recipes found!")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
@@ -127,8 +136,18 @@ public class BookmarkUseCaseTest {
         onView(withId(R.id.recipes)).perform(click());
         onView(withId(androidx.appcompat.R.id.search_src_text)).perform(replaceText("cake balls"), closeSoftKeyboard());
         onView(allOf(withId(androidx.appcompat.R.id.search_src_text), withText("cake balls"), childAtPosition(allOf(withId(androidx.appcompat.R.id.search_plate), childAtPosition(withId(androidx.appcompat.R.id.search_edit_frame), 1)), 0))).perform(pressImeActionButton());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(allOf(withId(R.id.recipe_card), childAtPosition(childAtPosition(withId(R.id.recipe_fragment_recycler_view), 0), 0), isDisplayed())).perform(click());
         onView(withId(R.id.add_to_bookmark_button)).perform(scrollTo(), click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(allOf(withId(R.id.radio_button_dialog_folder_list), withText("Desserts"), childAtPosition(childAtPosition(withId(R.id.recycler_view_bookmark_folder_list), 0), 0), isDisplayed())).perform(click());
         onView(withId(R.id.add_to_bookmark_confirm_button)).perform(click());
         onView(withText("Added recipe to Desserts")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
@@ -137,8 +156,18 @@ public class BookmarkUseCaseTest {
         onView(withId(R.id.recipes)).perform(click());
         onView(withId(androidx.appcompat.R.id.search_src_text)).perform(replaceText("APPLE PIE BARS"), closeSoftKeyboard());
         onView(allOf(withId(androidx.appcompat.R.id.search_src_text), withText("APPLE PIE BARS"), childAtPosition(allOf(withId(androidx.appcompat.R.id.search_plate), childAtPosition(withId(androidx.appcompat.R.id.search_edit_frame), 1)), 0))).perform(pressImeActionButton());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(allOf(withId(R.id.recipe_card), childAtPosition(childAtPosition(withId(R.id.recipe_fragment_recycler_view), 0), 0), isDisplayed())).perform(click());
         onView(withId(R.id.add_to_bookmark_button)).perform(scrollTo(), click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         onView(allOf(withId(R.id.radio_button_dialog_folder_list), withText("Desserts"), childAtPosition(childAtPosition(withId(R.id.recycler_view_bookmark_folder_list), 0), 0), isDisplayed())).perform(click());
         onView(withId(R.id.add_to_bookmark_confirm_button)).perform(click());
         onView(withText("Added recipe to Desserts")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
@@ -161,7 +190,7 @@ public class BookmarkUseCaseTest {
         onView(withId(R.id.profile)).perform(click());
         onView(withId(R.id.bookmarked_list_card)).perform(click());
         onView(allOf(withId(R.id.linear_folder_layout), childAtPosition(childAtPosition(withId(R.id.bookmark_list_recycler_view), 0), 0), isDisplayed())).perform(click());
-        onView(allOf(withId(R.id.folder_child_recycler_view), childAtPosition(withId(R.id.expandable_folder_layout), 0))).perform(actionOnItemAtPosition(0, click()));
+        onView(allOf(withId(R.id.folder_child_recycler_view), childAtPosition(withId(R.id.expandable_folder_layout), 0), hasChildCount(2))).perform(actionOnItemAtPosition(0, click()));
         onView(withId(R.id.add_to_bookmark_button)).perform(scrollTo(), click());
         onView(withId(R.id.dialog_add_to_bookmark_remove_button)).perform(click());
         onView(withText("Removed recipe from bookmark")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
@@ -171,7 +200,7 @@ public class BookmarkUseCaseTest {
         onView(withId(R.id.profile)).perform(click());
         onView(withId(R.id.bookmarked_list_card)).perform(click());
         onView(allOf(withId(R.id.linear_folder_layout), childAtPosition(childAtPosition(withId(R.id.bookmark_list_recycler_view), 0), 0), isDisplayed())).perform(click());
-        onView(allOf(withId(R.id.folder_child_recycler_view), childAtPosition(withId(R.id.expandable_folder_layout), 0), withText("Cake Balls"))).check(doesNotExist());
+        onView(allOf(withId(R.id.folder_child_recycler_view), childAtPosition(withId(R.id.expandable_folder_layout), 0), hasChildCount(1))).check(matches(isDisplayed()));
     }
 
     @Test
@@ -181,13 +210,38 @@ public class BookmarkUseCaseTest {
         onView(withId(R.id.profile)).perform(click());
         onView(withId(R.id.bookmarked_list_card)).perform(click());
         onView(allOf(withId(R.id.linear_folder_layout), childAtPosition(childAtPosition(withId(R.id.bookmark_list_recycler_view), 0), 0), isDisplayed())).perform(click());
-        onView(allOf(withId(R.id.folder_child_recycler_view), childAtPosition(withId(R.id.expandable_folder_layout), 0), withText("Apple Pie Bars")));
 
         // Click on garbage can icon button next to folder name “Desserts” -> “Confirm”
         // Check for correct toast message
         // Check that the folder is no longer in the list of folders
-        onView(allOf(withId(R.id.folder_delete_button), childAtPosition(childAtPosition(withId(R.id.linear_folder_layout), 0), 1), isDisplayed())).perform(click());
-        onView(allOf(withId(android.R.id.button1), withText("Confirm"), childAtPosition(childAtPosition(withClassName(is("android.widget.ScrollView")), 0), 3))).perform(scrollTo(), click());
+        onView(withId(R.id.bookmark_list_recycler_view))
+                .perform(
+                        RecyclerViewActions.actionOnItemAtPosition(
+                                0,
+                                new ViewAction() {
+                                    @Override
+                                    public Matcher<View> getConstraints() {
+                                        return null;
+                                    }
+
+                                    @Override
+                                    public String getDescription() {
+                                        return "Click on specific button";
+                                    }
+
+                                    @Override
+                                    public void perform(UiController uiController, View view) {
+                                        View button = view.findViewById(R.id.folder_delete_button);
+                                        button.performClick();
+                                    }
+                                })
+                );
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(allOf(withId(android.R.id.button1), withText("Confirm"))).perform(click());
         onView(withText("'Desserts' deleted")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
         onView(allOf(withId(R.id.bookmark_list_recycler_view), hasChildCount(0)));
 
@@ -239,20 +293,24 @@ public class BookmarkUseCaseTest {
         }
     }
 
-    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
-        return new TypeSafeMatcher<View>() {
-            int currentIndex = 0;
-
+    private static Matcher<View> getElementFromMatchAtPosition(final Matcher<View> matcher, final int position) {
+        return new BaseMatcher<View>() {
+            int counter = 0;
             @Override
-            public void describeTo(Description description) {
-                description.appendText("with index: ");
-                description.appendValue(index);
-                matcher.describeTo(description);
+            public boolean matches(final Object item) {
+                if (matcher.matches(item)) {
+                    if(counter == position) {
+                        counter++;
+                        return true;
+                    }
+                    counter++;
+                }
+                return false;
             }
 
             @Override
-            public boolean matchesSafely(View view) {
-                return matcher.matches(view) && currentIndex++ == index;
+            public void describeTo(final Description description) {
+                description.appendText("Element at hierarchy position "+position);
             }
         };
     }
