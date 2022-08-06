4f7b26df07de7dacd0ca88d6fc877f4753618c57
Ubuntu <azureuser@RecipEZ.wi1pxq02ziuutk5g4etwgxvdpa.px.internal.cloudapp.net>
2022-08-06 09:41:28 +0000

    code cleanup

--- backend/src/verify.js
+++ backend/src/verify.js
@@ -4,16 +4,10 @@ const CLIENT_ID = "158528567702-cla9vjg1b8mj567gnp1arb90870b001h.apps.googleuser
 const client = new OAuth2Client(CLIENT_ID);
 
 async function verify(token) {
-    const ticket = await client.verifyIdToken({
+    await client.verifyIdToken({
         idToken: token,
         audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
     });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-    // return new Promise((resolve, reject) => {
-    //     return resolve("mock verification")
-    // })
-
 }
 
 module.exports = {verify}
